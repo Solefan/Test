84ea41c6f2adc4f195cc2c680f7de3590089fdba
Sole <2606646771@qq.com>
2017-02-06 10:31:32 +0800

    更新方法

--- src/main/java/com/hlju/test/GenericTest.java
+++ src/main/java/com/hlju/test/GenericTest.java
@@ -14,12 +14,23 @@ public class GenericTest {
 
         System.out.println(name.getClass() == age.getClass());          //true
 
-
         getData(name);
         getData(age);
+
+        getData2(age);
+        getData3(age);
+
     }
 
     public static void getData(Box<?> data) {
+        System.out.println("data :" + data.getData());
+    }
+
+    public static void getData2(Box<? extends Number> data) {
+        System.out.println("data :" + data.getData());
+    }
+
+    public static void getData3(Box<? super Integer> data) {
         System.out.println("data :" + data.getData());
     }
 
