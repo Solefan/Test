c58f6629537cb5cae4d1e8865aa7d72d7062364e
Sole <2606646771@qq.com>
2017-02-10 10:23:51 +0800

    测试补码

---
+++ src/main/java/com/hlju/test/Tests.java
@@ -0,0 +1,14 @@
+package com.hlju.test;
+
+/**
+ * Created by Sole on 2017/2/9.
+ */
+public class Tests {
+
+    public static void main(String[] args) {
+        byte b = 127;
+        byte c = (byte)(b+1);
+        System.out.println(c);
+    }
+
+}
