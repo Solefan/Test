cd06b3618833f70a8e082db99e0b4f788ecff011
Sole <2606646771@qq.com>
2017-02-10 11:36:06 +0800

    addddddd

--- src/main/java/com/hlju/test/TTT.java
+++ src/main/java/com/hlju/test/TTT.java
@@ -4,4 +4,11 @@ package com.hlju.test;
  * Created by Sole on 2017/2/10.
  */
 public class TTT {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+
+    }
+
 }
