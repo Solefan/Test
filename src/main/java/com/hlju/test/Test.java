78e4f272506ad567babeedc729ea4b580c70f948
Sole <2606646771@qq.com>
2017-02-14 19:06:51 +0800

    添加Thread

--- src/main/java/com/hlju/test/Test.java
+++ src/main/java/com/hlju/test/Test.java
@@ -13,6 +13,8 @@ public class Test {
         method(a, b);
         System.out.println("a=" + a);
         System.out.println("b=" + b);
+        Thread t = new Thread();
+        t.start();
     }
 
     public static void method(final int a, final int b) {
